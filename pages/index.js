6e346f961e478d05dde1255ae3b7fa9b263e59a4
marshalldavid28 <137860345+marshalldavid28@users.noreply.github.com>
2025-01-11 20:10:24 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Welcome to SpeedMate!</h1>;
+  return (
+    <div>
+      <h1>Welcome to SpeedMate!</h1>
+      <nav>
+        <ul>
+          <li><Link href="/events"><a>View Events</a></Link></li>
+          <li><Link href="/auth"><a>Login/Signup</a></Link></li>
+          <li><Link href="/matches"><a>View Matches</a></Link></li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
